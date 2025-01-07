a5cde6fba7b9c1b2ed4e4fdbb1b48ebb63267657
tpsef <pseftelistheo@gmail.com>
2025-01-07 11:31:09 +0100

    Enable dark mode using class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit', // Enable JIT mode
+  darkMode: 'class',
+  mode: 'jit', 
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
