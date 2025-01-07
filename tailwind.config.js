5ce4640e1721aaa606084a188aa35aa8868ad3cf
tpsef <pseftelistheo@gmail.com>
2025-01-07 11:18:33 +0100

    Setup tailwind + hotrelaod dev server

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit', // Enable JIT mode
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    client: {
+      webSocketURL: 'ws://localhost:8080/ws',
+    },
+  },
 })
