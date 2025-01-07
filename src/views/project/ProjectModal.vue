9f5ded6532912606f9ab0b7d88dd90241694eaac
tpsef <pseftelistheo@gmail.com>
2025-01-07 20:43:25 +0100

    Update Add Project with modal

--- src/views/project/ProjectModal.vue
+++ src/views/project/ProjectModal.vue
@@ -9,9 +9,7 @@
     ]"
   >
     <!-- Overlay -->
-    <div
-      class="absolute inset-0 bg-gray-900 bg-opacity-70"
-    ></div>
+    <div class="absolute inset-0 bg-gray-900 bg-opacity-70"></div>
     <div class="relative p-4 w-full max-w-2xl max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -20,7 +18,7 @@
           class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
         >
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            Terms of Service
+            Create new project
           </h3>
 
           <button
@@ -47,38 +45,88 @@
             <span class="sr-only">Close modal</span>
           </button>
         </div>
-        <!-- Modal body -->
-        <div class="p-4 md:p-5 space-y-4">
-          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            With less than a month to go before the European Union enacts new
-            consumer privacy laws for its citizens, companies around the world
-            are updating their terms of service agreements to comply.
-          </p>
-          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            The European Union’s General Data Protection Regulation (G.D.P.R.)
-            goes into effect on May 25 and is meant to ensure a common set of
-            data rights in the European Union. It requires organizations to
-            notify users as soon as possible of high-risk data breaches that
-            could personally affect them.
-          </p>
-        </div>
+        <!-- INFO: Modal body -->
+        <form class="p-4 md:p-5">
+          <div class="grid gap-4 mb-4 grid-cols-2 text-left">
+            <div class="col-span-1">
+              <label
+                for="title"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Title</label
+              >
+              <input
+                v-model="newProject.name"
+                type="text"
+                name="name"
+                id="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required=""
+              />
+            </div>
+
+            <div class="col-span-2">
+              <label
+                for="description"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Description</label
+              >
+              <textarea
+                v-model="newProject.description"
+                id="description"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write product description here"
+              ></textarea>
+            </div>
+          </div>
+          <!-- INFO: Task List Start-->
+          <h2>Add new task</h2>
+          <div class="flex space-x-4 items-center">
+            <div class="relative z-0 w-1/2 mb-5 group">
+              <input
+                type="text"
+                name="task_name"
+                id="task_name"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+                v-model="newTask"
+              />
+              <label
+                for="task_name"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >New Task</label
+              >
+            </div>
+            <div>
+              <button
+                @click="addTask"
+                type="button"
+                class="px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <ul class="text-left">
+            <li v-for="task in newProject.taskList" :key="task">
+              {{ task.title }}
+            </li>
+          </ul>
+          <!-- INFO: Task List End-->
+        </form>
         <!-- Modal footer -->
         <div
           class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
         >
           <button
             data-modal-hide="default-modal"
+            @click="addProject"
             type="button"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            I accept
-          </button>
-          <button
-            data-modal-hide="default-modal"
-            type="button"
-            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            Decline
+            Add Project
           </button>
         </div>
       </div>
@@ -87,15 +135,39 @@
 </template>
 
 <script setup>
-import {  defineProps, defineEmits, onMounted } from "vue";
+import { ref, reactive, defineProps, defineEmits } from "vue";
+import { useProjectStore } from "@/stores/project";
 
-const emit = defineEmits(['closeModal']);
+defineProps(["isOpen"]);
+const emit = defineEmits(["closeModal"]);
 
-defineProps(['isOpen']);
+const project = useProjectStore();
+const newTask = ref("");
+const newProject = reactive({
+  name: "",
+  description: "",
+  taskList: [],
+});
 
-const closeModal = () => {
-  emit('closeModal');
+// Functions
+const addProject = () => {
+  project.addProject(newProject);
+  // Refresh Form
+  newProject.name = "";
+  newProject.description = "";
+  newProject.taskList = [];
+  newTask.value = ""
+  closeModal();
 };
-
-onMounted(() => {});
+const addTask = () => {
+  newProject.taskList.push({
+    id: "20",
+    title: newTask.value,
+    done: false,
+  });
+  newTask.value = "";
+};
+const closeModal = () => {
+  emit("closeModal");
+};
 </script>
--- src/views/project/ProjectsBoard.vue
+++ src/views/project/ProjectsBoard.vue
@@ -3,20 +3,13 @@
 
   <div class="flex p-4">
     <button
-      type="button"
-      class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-      @click="addProject"
-    >
-      Add project
-    </button>
-    <button
       data-modal-target="default-modal"
       data-modal-toggle="default-modal"
       type="button"
       class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       @click="openModal"
     >
-      ShowModal
+      Add project
     </button>
   </div>
   <div class="flex justify-start space-x-4">
@@ -40,30 +33,7 @@ import ProjectModal from "@/views/project/ProjectModal.vue";
 import { useProjectStore } from "@/stores/project";
 
 const project = useProjectStore();
-const addProject = () => {
-  project.addProject({
-    id: 3,
-    name: "Bike Dynamically Added",
-    description: "This is a project to organize the tasks about my bike setup",
-    taskList: [
-      {
-        title: "Fix flat tire",
-        description: "The rear tire is flat, I need to fix it",
-        status: "todo",
-      },
-      {
-        title: "Fix Brakes",
-        description: "Back brake's disk needs fixing",
-        status: "todo",
-      },
-      {
-        title: "Put oil in the chain",
-        description: "Add oil in the chain",
-        status: "todo",
-      },
-    ],
-  });
-};
+
 const isModalOpen = ref(false);
 const openModal = () => {
   isModalOpen.value = true;
