1519788c3251138d9c271ff80cf5e2a9c2d2903f
tpsef <pseftelistheo@gmail.com>
2025-01-09 17:52:24 +0100

    Update Form

--- src/components/CustomForm.vue
+++ src/components/CustomForm.vue
@@ -18,7 +18,7 @@
           type="text"
           name="name"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          placeholder="Type product name"
+          placeholder="Project Name"
           @keyup="errors.name = []"
         />
           <p
@@ -41,7 +41,7 @@
           id="description"
           rows="4"
           class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Write product description here"
+          placeholder="Project description"
         ></textarea>
       </div>
     </div>
@@ -80,14 +80,13 @@
 </template>
 
 <script setup>
-import { ref, defineEmits, reactive, defineProps, onMounted } from "vue";
+import { ref, reactive, defineProps, onMounted } from "vue";
 import { useProjectStore } from "@/stores/project";
 import TaskList from "@/components/TaskList.vue";
 import router from "@/router";
 
 const props = defineProps(["projectId", "action"]);
 const projectStore = useProjectStore();
-const emit = defineEmits(["closeModal"]);
 const currentProject = reactive({});
 const newTask = ref("");
 const errors = reactive({
@@ -124,10 +123,6 @@ const addProject = () => {
   projectStore.addProject(currentProject);
   currentProject.name = "";
   currentProject.description = "";
-  closeModal();
-};
-const closeModal = () => {
-  emit("closeModal");
 };
 
 const editProject = (projectId) => {
