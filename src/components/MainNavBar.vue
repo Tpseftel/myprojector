9a623ba4888cedd483afa2a3fef19200895d115c
tpsef <pseftelistheo@gmail.com>
2025-01-10 06:51:52 +0100

    Update

--- src/components/MainNavBar.vue
+++ src/components/MainNavBar.vue
@@ -54,13 +54,6 @@
             </router-link>
           </div>
           <div>
-            <a
-              href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Dark Mode</a
-            >
-          </div>
-          <div>
             <button
               type="button"
               @click="toggleTheme"
