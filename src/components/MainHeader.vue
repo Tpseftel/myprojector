017822ba29c4671d685d0effa1cde1e0d764e762
tpsef <pseftelistheo@gmail.com>
2025-01-10 09:37:50 +0100

    Update

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -7,7 +7,7 @@
       <div class="col-span-12 md:col-span-8  p-4 text-left">
        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">My Projector</h1>
         <p class="mb-6 text-lg font-normal dark:text-white px-3">
-            Organize your life in the most intuitive way..
+            Organize your tasks in the most intuitive way..
         </p>
       </div>
     </div>
